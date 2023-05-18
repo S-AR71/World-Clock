97b8367ae2fe63ae1ae5f7e9bb3b4283a1fc3227
S-AR71 <116632150+S-AR71@users.noreply.github.com>
2023-05-18 23:46:58 +0200

    JS function of Select

--- javascript/index.js
+++ javascript/index.js
@@ -1,38 +1,65 @@
 function updateTime() {
   // Seoul
   let seoulElement = document.querySelector("#seoul");
-  let seoulDateElement = seoulElement.querySelector(".date");
-  let seoulTimeElement = seoulElement.querySelector(".time");
-  let seoulTime = moment().tz("Asia/Seoul");
+  if (seoulElement) {
+    let seoulDateElement = seoulElement.querySelector(".date");
+    let seoulTimeElement = seoulElement.querySelector(".time");
+    let seoulTime = moment().tz("Asia/Seoul");
 
-  seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
-  seoulTimeElement.innerHTML = seoulTime.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  );
+    seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
+    seoulTimeElement.innerHTML = seoulTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Sydney
 
   let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyTime = moment().tz("Australia/Sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
 
-  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  );
+    sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Samoa
   let samoaElement = document.querySelector("#samoa");
-  let samoaDateElement = samoaElement.querySelector(".date");
-  let samoaTimeElement = samoaElement.querySelector(".time");
-  let samoaTime = moment().tz("Pacific/Samoa");
+  if (samoaElement) {
+    let samoaDateElement = samoaElement.querySelector(".date");
+    let samoaTimeElement = samoaElement.querySelector(".time");
+    let samoaTime = moment().tz("Pacific/Samoa");
 
-  samoaDateElement.innerHTML = samoaTime.format("MMMM Do YYYY");
-  samoaTimeElement.innerHTML = samoaTime.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  );
+    samoaDateElement.innerHTML = samoaTime.format("MMMM Do YYYY");
+    samoaTimeElement.innerHTML = samoaTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+  </div>
+  `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
